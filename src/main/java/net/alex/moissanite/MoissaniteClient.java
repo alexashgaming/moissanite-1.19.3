2bd4c145a747a3b5dad60d2322af94d26ebb1908
alexashgaming <alexashmaurivard@gmail.com>
2023-02-04 06:38:38 -0800

    fix

---
+++ src/main/java/net/alex/moissanite/MoissaniteClient.java
@@ -0,0 +1,10 @@
+package net.alex.moissanite;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoissaniteClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/alex/moissanite/MoissaniteModDataGenerator.java
@@ -0,0 +1,11 @@
+package net.alex.moissanite;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoissaniteModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
